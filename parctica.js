a12b5ffa539d3e38a22e38e9840bcf2d1ceadda7
EdwardQuevedoP <edwardquevedop@gmail.com>
2023-11-16 23:00:14 -0500

    hoja2

--- parctica.js
+++ parctica.js
@@ -1,11 +1,31 @@
-const readline = require('readline');
+const fs = require("fs")
+const http = require('http');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const PORT = 3000;
+// request
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    const nombre = req.url.split("?")[0]
+    const edad = req.url.split("?")[1]
+    const siNo = req.url.split("?")[2]
+    if(edad>=18){
+        res.end(nombre+ " "+ edad+" "+"es mayor de edad")
+    } else {
+        res.end(nombre+" "+edad+" "+"es menor de edad")
+    }
+    if(siNo=="si"){
+        fs.writeFileSync("prueba.txt","hola mundo")
+        console.log("funciona")
+    }
+    console.log(nombre)
+    console.log(edad)
+    console.log(siNo)
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found\n');
+  }
 });
 
-rl.question('¿Cómo te llamas? ', (answer) => {
-  console.log(`Hola , ${answer}!`);
-  rl.close();
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
